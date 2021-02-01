4033b3d38f7b867557b45369f71ebc6fc888da4e
SSCastanedaC <sscastanedac@gmail.com>
2021-02-01 02:09:10 -0500

    feat Crear variable DATABASES

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {}
-DATABASES['default'].update(db_from_env)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -85,7 +81,6 @@ WSGI_APPLICATION = 'pokedex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -96,7 +91,10 @@ DATABASES = {
         'PORT': 5432
     }
 }
-"""
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
