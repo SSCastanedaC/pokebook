25945d9f36523ff101fcfb11f34a4137a9ee3e13
SSCastanedaC <sscastanedac@gmail.com>
2021-02-01 01:28:02 -0500

    feat Cambiar credenciales de DB + Agregar REST a proyecto + Agregar utilidades de postgres

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.postgres',
+    'rest_framework',
     'apps.pokemon',
 ]
 
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'pokedex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pokedex',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
